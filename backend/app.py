08f204dac9fbe68002d2813f28df4018dadb8d45
Emil Rosin <SaltisRS>
2025-03-12 02:31:04 +0100

    fix ports

--- backend/app.py
+++ backend/app.py
@@ -54,4 +54,4 @@ async def get_ic_data() -> List[Dict]:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=80)
