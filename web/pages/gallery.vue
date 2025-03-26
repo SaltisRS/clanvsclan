a5110411f02bd3a16c9385ca73d4ce0580270e58
Emil Rosin <SaltisRS>
2025-03-26 23:50:58 +0100

    CMON CORS

--- web/pages/gallery.vue
+++ web/pages/gallery.vue
@@ -70,7 +70,7 @@ const getClanImage = (clan: "Iron Foundry" | "Ironclad") => {
 
 onMounted(async () => {
   try {
-    const response = await fetch("https://cvc-backend:8001/gallery");
+    const response = await fetch("http://cvc-backend:8001/gallery");
     const data = await response.json();
     
     if (Array.isArray(data)) {
