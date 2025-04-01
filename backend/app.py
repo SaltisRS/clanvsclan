bd4b62fc0f9b0287938559bbebe7091662d5481c
Emil Rosin <SaltisRS>
2025-04-01 23:17:16 +0200

    Removed unused deps

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,8 @@ from pymongo import MongoClient
 from typing import List, Dict, Literal
 from loguru import logger
 import os
-import httpx
 from dotenv import load_dotenv
-from uuid import uuid4
-from pathlib import Path
-from PIL import Image
-from io import BytesIO
+
 
 
 load_dotenv()
@@ -45,27 +41,6 @@ async def get_ic_data() -> List[Dict]:
     logger.info(f"Data retrieved: {data}")
     return data
 
-@app.post("/saveimage")
-async def save_image(image_url: str, clan: Literal["IF", "IC"]):
-    with httpx.Client() as client:
-        response = client.get(image_url)
-        response.raise_for_status()
-    
-    image = Image.open(BytesIO(response.content))
-    extension = image.format
-    if extension:
-        path = Path(".") / "gallery" / f"{str(uuid4())}_{clan}.{extension.lower()}"
-        image.save(path)
-
-
-@app.get("/leaderboard")
-async def get_leaderboard() -> Dict[str, str | int]:
-    """Fetches Competition from WOM and formats it."""
-    competition_url = "https://wiseoldman.com/api/v2/competitions..."
-    response = httpx.get(competition_url)
-    if response.status_code == 200:
-        return {}
-    return {}
 
 if __name__ == "__main__":
     import uvicorn
