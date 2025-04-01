db8593592e9f7c4f3220b7ee348e85490280c61e
Emil Rosin <SaltisRS>
2025-04-01 21:48:24 +0200

    Weird Array

--- discordbot/client/commands/template.py
+++ discordbot/client/commands/template.py
@@ -361,9 +361,10 @@ async def create_source_options(tier: str) -> list[discord.SelectOption]:
     template: dict | None = await coll.find_one({})
     if not template: return [discord.SelectOption(label="No Data Matched")]
     
-    for source in template["tiers"][tier]["sources"]:
-        options.append(discord.SelectOption(label=source["name"], value=source["name"]))
-        logger.info(f"Adding '{source}' to options.")
+    for idx in template["tiers"][tier]["sources"]:
+        for source in idx:
+            options.append(discord.SelectOption(label=source["name"], value=source["name"]))
+            logger.info(f"Adding '{source["name"]}' to options.")
     
     return options
 
