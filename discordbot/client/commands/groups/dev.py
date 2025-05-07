f8f55e95444fd47d0d03e7cd4e2f39e7a80d6df0
Emil Rosin <SaltisRS>
2025-05-08 00:45:35 +0200

    Lowercase smh

--- discordbot/client/commands/groups/dev.py
+++ discordbot/client/commands/groups/dev.py
@@ -4,4 +4,4 @@ from discord import app_commands
 
 class DevGroup(app_commands.Group):
     def __init__(self):
-        super().__init__(name="Dev", description="Development group")
+        super().__init__(name="dev", description="Development group")
