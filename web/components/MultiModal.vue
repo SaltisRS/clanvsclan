c87685b4b480115e06cb3dbfe6c050936f006a6e
Emil Rosin <SaltisRS>
2025-05-24 00:51:25 +0200

    Inset

--- web/components/MultiModal.vue
+++ web/components/MultiModal.vue
@@ -1,11 +1,11 @@
 <template>
 
-    <div v-if="isVisible" class="fixed inset-2 bg-dc-accent bg-opacity-95 flex justify-center items-center z-50">
+    <div v-if="isVisible" class="fixed inset-10 bg-dc-accent bg-opacity-95 flex justify-center items-center z-50">
       <div class="bg-dc-background p-6 rounded-lg w-full max-w-xl max-h-[90vh] overflow-y-auto">
-        <h3 class="text-xl pt-4 font-semibold mb-6 text-white">Multipliers</h3>
+        <h3 class="absolute text-xl top-8 left-4 font-semibold mb-6 text-white">Multipliers</h3>
   
         <div v-if="multipliers && multipliers.length > 0">
-          <div v-for="(multiplier, index) in multipliers" :key="index" class="border-b border-dc-accent pb-4 mb-4 last:border-b-0 last:mb-0">
+          <div v-for="(multiplier, index) in multipliers" :key="index" class="border-b w-full border-dc-accent pb-4 mb-4 last:border-b-0 last:mb-0">
             <div class="font-bold text-white">{{ multiplier.name }}</div>
             <div class="text-sm text-gray-300 mb-2">{{ multiplier.description }}</div>
             <div class="text-sm text-white">Factor: {{ multiplier.factor }}x</div>
